d83247b9909ed6dcf9090179d541ea4538c0f9f7
Jarawadee <jarawadee0414@gmail.com>
2025-06-23 16:54:56 +0700

    Update ev_microscopic.py

--- ev_microscopic.py
+++ ev_microscopic.py
@@ -4,4 +4,4 @@ st.write('EV')
 st.write('สวัสดี')
 
 st.badge("Home", color="blue")
-st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, *, type="secondary", icon=None, disabled=False, use_container_width=False)
+
