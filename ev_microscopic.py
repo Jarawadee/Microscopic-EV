435fdeed822c511c3b6c21dcb116522e5186e22a
Jarawadee <jarawadee0414@gmail.com>
2025-06-26 22:15:38 +0700

    Update ev_microscopic.py

--- ev_microscopic.py
+++ ev_microscopic.py
@@ -5,9 +5,6 @@ import tensorflow as tf
 from PIL import Image
 
 #--------------------------------------------------------------------------------------------------------------
-
-#--------------------------------------------------------------------------------------------------------------
-
 model_path = 'ev_cnn_mobile.keras'
 model = tf.keras.models.load_model(model_path, custom_objects={'mse': tf.keras.losses.MeanSquaredError()})
     
