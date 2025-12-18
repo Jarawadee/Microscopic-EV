3ced3f6b3d2ff532b22c29a2de24827e7f212376
Jarawadee <jarawadee0414@gmail.com>
2025-12-18 20:00:40 +0700

    Update ev_microscopic.py

--- ev_microscopic.py
+++ ev_microscopic.py
@@ -32,7 +32,7 @@ class_label = ["Artifact", "Ev eggs"]
 
 def drawbox(img, label, a, b, c, d, color):
     image = cv2.rectangle(img, (c, a), (d, b), color, 3)
-    image = cv2.putText(image, label, (c, a - 10), cv2.FONT_HERSHEY_TRIPLEX, 1, color, 3)
+    image = cv2.putText(image, label, (c, a - 10), cv2.FONT_HERSHEY_TRIPLEX, 3, color, 3)
     return image
 
 def compute_iou(box1, box2):
